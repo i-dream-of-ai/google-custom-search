86594fb59577f7e8895c2a670029e1b123092ed3
Richard-Weiss <76665424+Richard-Weiss@users.noreply.github.com>
2025-01-24 20:20:14 +0100

    feat: use lowlevel server (#3)

--- src/mcp_google_cse/server.py
+++ src/mcp_google_cse/server.py
@@ -1,20 +1,59 @@
 import os
+from sys import stdin, stdout
 from typing import Any, List
 
 from googleapiclient.discovery import build
-from mcp.server.fastmcp import FastMCP
+from mcp import Tool, stdio_server
+from mcp.server.lowlevel import Server
+from mcp.types import TextContent, ImageContent, EmbeddedResource
 
-mcp = FastMCP("mcp-google-cse")
+stdin.reconfigure(encoding='utf-8')
+stdout.reconfigure(encoding='utf-8')
+
+server = Server("mcp-google-cse")
 
 
-@mcp.tool()
-def google_search(search_term: str) -> Any:
+@server.list_tools()
+async def handle_list_tools() -> list[Tool]:
+    """
+    List available tools.
+    Each tool specifies its arguments using JSON Schema validation.
+    """
+    return [
+        Tool(
+            name="google_search",
+            description="Search the custom search engine using the search term. Regular query arguments can also be used, like appending site:reddit.com or after:2024-04-30. If available and/or requested, the links of the search results should be used in a follow-up request using a different tool to get the full content. Example: \"claude.ai features site:reddit.com after:2024-04-30\"",
+            inputSchema={
+                "type": "object",
+                "properties": {
+                    "search_term": {"type": "string"},
+                },
+                "required": ["search_term"],
+            }
+        )
+    ]
+
+
+@server.call_tool()
+async def handle_call_tool(
+        name: str, arguments: dict[str, Any] | None
+) -> list[TextContent | ImageContent | EmbeddedResource]:
+    """
+    Handle tool execution requests.
+    Tools can modify server state and notify clients of changes.
+    """
+    search_term = arguments.get('search_term', '')
+
+    if name == 'google_search' and search_term != '':
+        result = await google_search(search_term)
+        return result
+    else:
+        raise ValueError(f"Unknown tool: {name}")
+
+
+async def google_search(search_term: str) -> List[TextContent]:
     """
     Search the custom search engine using the search term.
-    Regular query arguments can also be used, like appending site:reddit.com or after:2024-04-30.
-    If available and/or requested, the links of the search results should be used
-    in a follow-up request using a different tool to get the full content.
-    Example: "claude.ai features site:reddit.com after:2024-04-30"
     :param search_term: The search term to search for, equaling the q argument in Google's search.
     :return: Search results containing the title, link and snippet of the search result.
     """
@@ -29,7 +68,18 @@ def google_search(search_term: str) -> Any:
         fields='items(title,link,snippet)').execute()
     results = response['items']
     __clean_up_snippets(results)
-    return results
+
+    text_contents = []
+
+    for result in results:
+        text_contents.append(
+            TextContent(
+                type="text",
+                text=f"Title: {result['title']}\nLink: {result['link']}\nSnippet: {result['snippet']}"
+            )
+        )
+
+    return text_contents
 
 
 def __clean_up_snippets(items: List[dict]) -> None:
@@ -42,5 +92,11 @@ def __clean_up_snippets(items: List[dict]) -> None:
         item.update({k: v.replace('\xa0', ' ').strip() if k == 'snippet' else v for k, v in item.items()})
 
 
-def main():
-    mcp.run()
+async def run() -> None:
+    options = server.create_initialization_options()
+    async with stdio_server() as (read_stream, write_stream):
+        await server.run(
+            read_stream,
+            write_stream,
+            options
+        )
